e502d5dabd119aa7c873eb52cba02fce4f84df67
original-name0 <81045228+original-name0@users.noreply.github.com>
2025-04-13 13:12:03 +0200

    Add files via upload

--- semestralnaPraca-0_2.py
+++ semestralnaPraca-0_2.py
@@ -1,14 +1,14 @@
-from tkinter import *
-from random import randint
+import tkinter as tk
+from random import sample, choice, randint
+from collections import Counter
 from PIL import Image, ImageTk, ImageSequence
-import time
 
-win = Tk()
+win = tk.Tk()
 
 # config
 nameOfGame          =   "semestralnaPraca"
-winSizeX            =   600
-winSizeY            =   400
+winSizeX            =   800
+winSizeY            =   600
 
 # setup
 win.resizable(width=False, height=False)
@@ -20,10 +20,19 @@ win.geometry(winSize)
 fntMenuBtn          =   ("PX Sans Nouveaux", 11)
 fntMenu             =   ("PX Sans Nouveaux", 15)
 fntInGame           =   ("PX Sans Nouveaux", 12)
+fntInGameLettets    =   ("PX Sans Nouveaux", 10)
 
 # gameAttributes
-healthAttributePlay =   11
-healthAttributeEnem =   10
+vowelsShort         =   ["a", "e", "i", "y", "o", "u", "ä"]
+vowelsAndDiphthong  =   ["á", "é", "í", "ó", "ú", "ý", "ô"]
+consonantsBothLike  =   ["b", "m", "p", "r", "s", "v", "z", "f"]
+consonantsHard      =   ["h", "c", "h", "k", "g", "d", "t", "n", "l"]
+consonantsSoft      =   ["č", "d", "ž", "š", "ž", "c", "d", "z", "j", "ď", "ť", "ň", "ľ"]
+
+lettersInUse        =   []
+attackBind          =   "<Return>"
+healthAttributePlay =   16
+healthAttributeEnem =   3
 levelAttribute      =   1
 mainDelay           =   500
 namesOfEnemy        =   ["Skeleton", "Slime", "Bandint", "Spider", "Bat"]
@@ -55,10 +64,11 @@ def mainMenu():
     # reset
     returnToFirstLv()
     healPlayer()
+    deleteText()
     # placeObjects
-    nameGame.place(x=winSizeX/2, y=(winSizeY/2)-40, anchor=CENTER)
-    startButton.place(x=winSizeX/2, y=(winSizeY/2)+20, anchor=CENTER)
-    endButton.place(x=winSizeX/2, y=(winSizeY/2)+80, anchor=CENTER)
+    nameGame.place(x=winSizeX/2, y=(winSizeY/2)-40, anchor=tk.CENTER)
+    startButton.place(x=winSizeX/2, y=(winSizeY/2)+20, anchor=tk.CENTER)
+    endButton.place(x=winSizeX/2, y=(winSizeY/2)+80, anchor=tk.CENTER)
 
 # closeFunction
 def endGameButton():
@@ -71,36 +81,41 @@ def startGameButton():
     
     # playerTakesDamage
     playerTakesDamage()
+    win.bind(attackBind, playerAttack)
+    lettersAdder()
     
     # placeObjects
-    attackButton.place(x=winSizeX/2, y=winSizeY-40, anchor=CENTER)
-    playerHealthPoints.place(x=winSizeX/2+((winSizeX/2)/2)+33, y=(winSizeY/2)+((winSizeY/2)/2)+50, anchor=CENTER)
-    playerWriter.place(x=((winSizeX/2)/2)-25, y=(winSizeY/2)+((winSizeY/2)/2)+50, anchor=CENTER)
-    playerLetters.place(x=winSizeX/2, y=60, anchor=CENTER)
-    enemyHealthPoints.place(x=winSizeX/2, y=20, anchor=CENTER)
-    enemyName.place(x=winSizeX/2, y=60, anchor=CENTER)
-    levelIndication.place(x=winSizeX-50, y=10, anchor=NE)
+    attackButton.place(x=winSizeX/2, y=winSizeY-40, anchor=tk.CENTER)
+    playerHealthPoints.place(x=winSizeX/2+((winSizeX/2)/2)+33, y=(winSizeY/2)+((winSizeY/2)/2)+50, anchor=tk.CENTER)
+    playerWriter.place(x=((winSizeX/2)/2)-25, y=(winSizeY/2)+((winSizeY/2)/2)+50, anchor=tk.CENTER)
+    playerLetters.place(x=winSizeX/2, y=winSizeY-80, anchor=tk.CENTER)
+    enemyHealthPoints.place(x=winSizeX/2, y=20, anchor=tk.CENTER)
+    enemyName.place(x=winSizeX/2, y=60, anchor=tk.CENTER)
+    levelIndication.place(x=winSizeX-50, y=10, anchor=tk.NE)
 
 # endFightMenuFunction
 def afterFightMenu():
     global firstEnemyHealth, playerHealth
     # playerWon
     if firstEnemyHealth <= 0:
-        youWonTitle.place(x=winSizeX/2, y=(winSizeY/2)-30, anchor=CENTER)
-        backToMenuButton.place(x=(winSizeX/2)-100, y=(winSizeY/2)+20, anchor=CENTER)
-        nextLvButton.place(x=(winSizeX/2)+100, y=(winSizeY/2)+20, anchor=CENTER)
+        win.unbind(attackBind)
+        youWonTitle.place(x=winSizeX/2, y=(winSizeY/2)-30, anchor=tk.CENTER)
+        backToMenuButton.place(x=(winSizeX/2)-100, y=(winSizeY/2)+20, anchor=tk.CENTER)
+        nextLvButton.place(x=(winSizeX/2)+100, y=(winSizeY/2)+20, anchor=tk.CENTER)
         attackButton.config(state="disabled")
         playerWriter.config(state="disabled")
     # playerLost
     elif playerHealth <= 0:
-        youLostTitle.place(x=winSizeX/2, y=(winSizeY/2)-30, anchor=CENTER)
-        backToMenuButton.place(x=(winSizeX/2), y=(winSizeY/2)+30, anchor=CENTER)
+        win.unbind(attackBind)
+        youLostTitle.place(x=winSizeX/2, y=(winSizeY/2)-30, anchor=tk.CENTER)
+        backToMenuButton.place(x=(winSizeX/2), y=(winSizeY/2)+30, anchor=tk.CENTER)
         attackButton.config(state="disabled")
         playerWriter.config(state="disabled")
 
 # resetForReturnToMainMenu
 def returnToFirstLv():
-    global firstEnemyHealth, level, nextEnemyHealth
+    global firstEnemyHealth, level, nextEnemyHealth, lettersInUse
+    lettersInUse = []
     level = levelAttribute
     firstEnemyHealth = healthAttributeEnem
     nextEnemyHealth = healthAttributeEnem
@@ -113,7 +128,9 @@ def returnToFirstLv():
 def nextLv():
     global level, firstEnemyHealth, nextEnemyHealth
     # healsPlayer
+    win.bind(attackBind, playerAttack)
     healPlayer()
+    deleteText()
     
     # nextEnemyAndCounterOfLevel
     level += 1
@@ -141,9 +158,10 @@ def healPlayer():
     playerHealth = healthAttributePlay
 
 # takesHealthFromEnemy
-def playerAttack():
+def playerAttack(event=None):
     global firstEnemyHealth
-    damage = 3
+    damage = damagePerLetter()
+    deleteText()
     firstEnemyHealth -= damage
     enemyHealthPoints.config(text=str(firstEnemyHealth))
     # endFightMenuFunction
@@ -155,33 +173,73 @@ def playerTakesDamage():
     if playerHealth > 0 and firstEnemyHealth > 0:
         playerHealth -= 1
         playerHealthPoints.config(text=str(playerHealth))
-        win.after(1000, playerTakesDamage)
+        win.after(1500, playerTakesDamage)
     # endFightMenuFunction
     afterFightMenu()
 
 def damagePerLetter():
-    pass
+    wordInWriter = playerWriter.get()
+    #letersInDesposal = "aeio"
+    damage = len(wordInWriter)
+    words = ["name"]
+    if damage <= 2:
+        return False
 
+    if wordInWriter not in words:
+        return False
+
+    # Skontroluj, či sú všetky písmená dostupné v zásobe
+    lettersInFound = Counter(lettersInUse)
+    lettersInWriter = Counter(wordInWriter)
+
+    for letter, count in lettersInWriter.items():
+        if lettersInFound[letter] < count:
+            return False
+    
+    return damage
+
+def deleteText():
+    playerWriter.delete(0, tk.END)
+
+def randomizer():
+    global lettersInUse
+    lettersInUse.extend(sample(vowelsShort, 3))
+
+
+    lettersInUse.append(choice(vowelsAndDiphthong))
+    lettersInUse.extend(sample(consonantsBothLike, 2))
+    lettersInUse.extend(sample(consonantsHard, 2))
+    lettersInUse.extend(sample(consonantsSoft, 2))
+    
+    return lettersInUse
+        
+def lettersAdder():
+    global level, lettersInUse
+    arrayOfLetters = randomizer()
+    playerLetters.config(text=arrayOfLetters)
+    print(arrayOfLetters)
+    return arrayOfLetters
+    
 # objects on screen
 # mainMenu !!!
-nameGame            =   Label(win,text=nameOfGame,font=fntMenu)
-startButton         =   Button(win,text="start",font=fntMenuBtn,command=delayStartGame,bg="grey",fg="white", width=9)
-endButton           =   Button(win,text="close",font=fntMenuBtn,command=endGameButton,bg="grey",fg="white", width=9)
+nameGame            =   tk.Label(win,text=nameOfGame,font=fntMenu)
+startButton         =   tk.Button(win,text="Start",font=fntMenuBtn,command=delayStartGame,bg="grey",fg="white", width=9)
+endButton           =   tk.Button(win,text="Close",font=fntMenuBtn,command=endGameButton,bg="grey",fg="white", width=9)
 
 # fightEndedMenu !!!
-youWonTitle         =   Label(win,text="you_defeated_enemy",font=fntMenu, fg="blue")
-youLostTitle        =   Label(win,text="you_lost",font=fntMenu, fg="red")
-backToMenuButton    =   Button(win, command=delayStartMenu, text="Back", font=fntMenuBtn, bg="blue", fg="white")
-nextLvButton        =   Button(win, command=delayNextLevel, text="Next", font=fntMenuBtn, bg="blue", fg="white")
+youWonTitle         =   tk.Label(win,text="youDefeatedEnemy",font=fntMenu, fg="blue")
+youLostTitle        =   tk.Label(win,text="youLost",font=fntMenu, fg="red")
+backToMenuButton    =   tk.Button(win, command=delayStartMenu, text="Back", font=fntMenuBtn, bg="blue", fg="white")
+nextLvButton        =   tk.Button(win, command=delayNextLevel, text="Next", font=fntMenuBtn, bg="blue", fg="white")
 
 # gameStarted !!!
-attackButton        =   Button(win,command=playerAttack,text="Attack", font=fntInGame, bg="red", fg="white")
-playerHealthPoints  =   Label(win,text=str(playerHealth)+" hp",font=fntInGame,fg="red")
-playerWriter        =   Entry(win, font=fntInGame, width=10)
-playerLetters       =   Label(win)
-enemyHealthPoints   =   Label(win,text=str(firstEnemyHealth),font=fntInGame,fg="red")
-enemyName           =   Label(win,text=namesOfEnemy[randint(0, 3)], font=fntInGame)
-levelIndication     =   Label(win,text=str(level)+"LV",font=fntInGame, fg="black")
+attackButton        =   tk.Button(win,command=playerAttack,text="Attack", font=fntInGame, bg="red", fg="white")
+playerHealthPoints  =   tk.Label(win,text=str(playerHealth)+" hp",font=fntInGame,fg="red")
+playerWriter        =   tk.Entry(win, font=fntInGame, width=10)
+playerLetters       =   tk.Label(win,text=lettersInUse,font=fntInGameLettets)
+enemyHealthPoints   =   tk.Label(win,text=str(firstEnemyHealth),font=fntInGame,fg="red")
+enemyName           =   tk.Label(win,text=namesOfEnemy[randint(0, 3)], font=fntInGame)
+levelIndication     =   tk.Label(win,text=str(level)+"LV",font=fntInGame, fg="black")
 
 # mainLoop !!!
 mainMenu()
